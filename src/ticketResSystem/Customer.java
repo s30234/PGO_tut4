0295c194a2e74e82d8f29e50edf130ebbd7f62b8
s30234 <s30234@pjwstk.edu.pl>
2025-03-28 15:00:00 +0100

    First commit including Customer Class and Event Class

---
+++ src/ticketResSystem/Customer.java
@@ -0,0 +1,86 @@
+package ticketResSystem;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private ArrayList<Event>reservationList;
+
+
+    private Customer(String firstName, String lastName, String email, ArrayList<Event>reservationList) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+        setReservationList(reservationList);
+    }
+
+    private Customer(String firstName, String lastName) {
+        this(firstName, lastName,"",null);
+    }
+
+
+    private void addReservation(Event event){
+        reservationList.add(event);
+    }
+
+    public void displayReservations() {
+        if(reservationList.isEmpty()){
+            System.out.println("No reservations for this event");
+        }else {
+            for(Event event : reservationList){
+                System.out.println(event);
+            }
+        }
+    }
+
+    public void cancelReservation(Event event){
+        reservationList.remove(event);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", reservationList=" + reservationList +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Event> getReservationList() {
+        return reservationList;
+    }
+
+    public void setReservationList(ArrayList<Event> reservationList) {
+        this.reservationList = reservationList;
+    }
+
+
+}
